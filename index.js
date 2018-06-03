ccec96c21512bedf2d2fc24d13f20bce2ab2dc13
Emmanuel Odorisio <odorisioe@gmail.com>
2018-06-04 02:53:49 +1200

    Default functionality

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+    plugin: require('./lib/plugin'),
+    sign: require('./lib/sign'),
+    verify: require('./lib/verify'),
+    decode: require('./lib/decode')
+};

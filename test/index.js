e39eaa62bede478058ec218bbc20e00e53ffd61b
Emmanuel Odorisio <odorisioe@gmail.com>
2018-06-04 02:55:04 +1200

    Add 13 tests

---
+++ test/index.js
@@ -0,0 +1,574 @@
+'use strict';
+
+const Code = require('code');
+const Hoek = require('hoek');
+const HapiJWT = require('../');
+const Hapi = require('hapi');
+const Lab = require('lab');
+
+const { describe, it } = exports.lab = Lab.script();
+const expect = Code.expect;
+
+const internals = {};
+
+/**
+ * Tests
+ *
+ */
+describe('hapi-jsonwebtoken', () => {
+
+
+    it('Encode, decode & verify - valid token', () => {
+
+        const config = {
+            secretOrPrivateKey: 's3cr3t'
+        };
+        const user = internals.users[1];
+        const token = HapiJWT.sign(user, config);
+        const decoded = HapiJWT.decode(token);
+
+        expect(decoded.id).to.equal(user.id);
+        expect(decoded.scope).to.equal(user.scope);
+        expect(decoded.username).to.equal(user.username);
+
+        const verified = HapiJWT.verify(token, config);
+
+        expect(verified.id).to.equal(user.id);
+        expect(verified.scope).to.equal(user.scope);
+        expect(verified.username).to.equal(user.username);
+    });
+
+
+    it('Encode, decode & verify - valid token with config', () => {
+
+        const config = Hoek.clone(internals.config[1]);
+        const user = internals.users[2];
+        const token = HapiJWT.sign(user, config);
+
+        const decoded = HapiJWT.decode(token);
+
+        expect(decoded.id).to.equal(user.id);
+        expect(decoded.scope).to.equal(user.scope);
+        expect(decoded.username).to.equal(user.username);
+
+        const verified = HapiJWT.verify(token, config);
+
+        expect(verified.id).to.equal(user.id);
+        expect(verified.scope).to.equal(user.scope);
+        expect(verified.username).to.equal(user.username);
+
+    });
+
+
+    it('Encode, decode & verify - expired token with config', () => {
+
+        const config = Hoek.clone(internals.config[1]);
+        config.sign.options.expiresIn = -10;
+        const user = internals.users[3];
+        const token = HapiJWT.sign(user, config);
+
+        const decoded = HapiJWT.decode(token);
+
+        expect(decoded.id).to.equal(user.id);
+        expect(decoded.scope).to.equal(user.scope);
+        expect(decoded.username).to.equal(user.username);
+
+        let error;
+        try {
+            HapiJWT.verify(token, config);
+        }
+        catch (err) {
+            error = err;
+        }
+
+        expect(error.name).to.equal('TokenExpiredError');
+    });
+
+
+    it('Encode, decode & verify (w/ promises) - valid token', async () => {
+
+        const config = {
+            secretOrPrivateKey: 's3cr3t',
+            sign: {
+                promise: true
+            },
+            decode: {
+                promise: true
+            },
+            verify: {
+                promise: true
+            }
+        };
+        const user = internals.users[1];
+        const token = await HapiJWT.sign(user, config)
+            .then((result) => {
+
+                return result;
+            });
+        const decoded = await HapiJWT.decode(token, config)
+            .then((result) => {
+
+                return result;
+            });
+
+        expect(decoded.id).to.equal(user.id);
+        expect(decoded.scope).to.equal(user.scope);
+        expect(decoded.username).to.equal(user.username);
+
+        const verified = await HapiJWT.verify(token, config)
+            .then((result) => {
+
+                return result;
+            });
+
+        expect(verified.id).to.equal(user.id);
+        expect(verified.scope).to.equal(user.scope);
+        expect(verified.username).to.equal(user.username);
+    });
+
+
+    it('Encode, decode & verify (w/ promises) - valid token with config', async () => {
+
+        const config = Hoek.clone(internals.config[1]);
+        config.sign.promise = true;
+        config.decode.promise = true;
+        config.verify.promise = true;
+
+        const user = internals.users[2];
+        const token = await HapiJWT.sign(user, config)
+            .then((result) => {
+
+                return result;
+            });
+        const decoded = await HapiJWT.decode(token, config)
+            .then((result) => {
+
+                return result;
+            });
+
+        expect(decoded.payload.id).to.equal(user.id);
+        expect(decoded.payload.scope).to.equal(user.scope);
+        expect(decoded.payload.username).to.equal(user.username);
+
+        const verified = await HapiJWT.verify(token, config)
+            .then((result) => {
+
+                return result;
+            });
+
+        expect(verified.id).to.equal(user.id);
+        expect(verified.scope).to.equal(user.scope);
+        expect(verified.username).to.equal(user.username);
+    });
+
+
+    it('Encode, decode & verify (w/ promises) - expired token with config', async () => {
+
+        const config = Hoek.clone(internals.config[1]);
+        config.sign.promise = true;
+        config.sign.options.expiresIn = -10;
+        config.decode.promise = true;
+        config.verify.promise = true;
+
+        const user = internals.users[2];
+        const token = await HapiJWT.sign(user, config)
+            .then((result) => {
+
+                return result;
+            });
+        const decoded = await HapiJWT.decode(token, config)
+            .then((result) => {
+
+                return result;
+            });
+
+        expect(decoded.payload.id).to.equal(user.id);
+        expect(decoded.payload.scope).to.equal(user.scope);
+        expect(decoded.payload.username).to.equal(user.username);
+
+        const error = await HapiJWT.verify(token, config)
+            .then((result) => {
+
+                return result;
+            })
+            .catch((err) => {
+
+                return err;
+            });
+
+        expect(error.name).to.equal('TokenExpiredError');
+    });
+
+
+    it('Auth plugin - unauthorized (no token)', async () => {
+
+        const server = await internals.hapi();
+        server.auth.strategy('jwt', 'hapi-jsonwebtoken', internals.config[1]);
+
+        server.route({
+            method: 'GET',
+            path: '/',
+            handler: function (request, h) {
+
+                return 'OK';
+            },
+            options: {
+                auth: 'jwt'
+            }
+        });
+
+        const request = { method: 'GET', url: '/' };
+        const res = await server.inject(request);
+
+        expect(res.result).not.equal('OK');
+        expect(res.statusCode).to.equal(401);
+    });
+
+
+    it('Auth plugin - unauthorized (no scope)', async () => {
+
+        const user = internals.users[3];
+        const config = Hoek.clone(internals.config[1]);
+        const token = await HapiJWT.sign(user, config);
+
+        const server = await internals.hapi();
+        server.auth.strategy('jwt', 'hapi-jsonwebtoken', config);
+
+        server.route({
+            method: 'GET',
+            path: '/',
+            handler: function (request, h) {
+
+                return 'OK';
+            },
+            options: {
+                auth: {
+                    strategy: 'jwt',
+                    scope: ['admin']
+                }
+            }
+        });
+
+        const request = { method: 'GET', url: '/', headers: { authorization: 'Bearer ' + token } };
+        const res = await server.inject(request);
+
+        expect(res.result).not.equal('OK');
+        expect(res.statusCode).to.equal(403);
+    });
+
+
+    it('Auth plugin - unauthorized (invalid token)', async () => {
+
+        const user = internals.users[3];
+        const config = Hoek.clone(internals.config[1]);
+        const token = await HapiJWT.sign(user, config);
+
+        const server = await internals.hapi();
+        server.auth.strategy('jwt', 'hapi-jsonwebtoken', config);
+
+        server.route({
+            method: 'GET',
+            path: '/',
+            handler: function (request, h) {
+
+                return 'OK';
+            },
+            options: {
+                auth: {
+                    strategy: 'jwt',
+                    scope: ['user']
+                }
+            }
+        });
+
+        const request = { method: 'GET', url: '/', headers: { authorization: 'Bearer ' + 'abcde' + token.slice(5) } };
+        const res = await server.inject(request);
+
+        expect(res.result).not.equal('OK');
+        expect(res.statusCode).to.equal(401);
+    });
+
+
+    it('Auth plugin - unauthorized (invalid user)', async () => {
+
+        const user = internals.users[4];
+        const config = Hoek.clone(internals.config[1]);
+        const token = await HapiJWT.sign(user, config);
+
+        const server = await internals.hapi();
+        server.auth.strategy('jwt', 'hapi-jsonwebtoken', config);
+
+        server.route({
+            method: 'GET',
+            path: '/',
+            handler: function (request, h) {
+
+                return 'OK';
+            },
+            options: {
+                auth: {
+                    strategy: 'jwt',
+                    scope: ['user']
+                }
+            }
+        });
+
+        const request = { method: 'GET', url: '/', headers: { authorization: 'Bearer ' + token } };
+        const res = await server.inject(request);
+
+        expect(res.result).not.equal('OK');
+        expect(res.statusCode).to.equal(401);
+    });
+
+
+    it('Auth plugin - unauthorized (expired token)', async () => {
+
+        const user = internals.users[1];
+        const config = Hoek.clone(internals.config[1]);
+        config.sign.options.expiresIn = -10;
+        const token = await HapiJWT.sign(user, config);
+
+        const server = await internals.hapi();
+        server.auth.strategy('jwt', 'hapi-jsonwebtoken', config);
+
+        server.route({
+            method: 'GET',
+            path: '/',
+            handler: function (request, h) {
+
+                return 'OK';
+            },
+            options: {
+                auth: {
+                    strategy: 'jwt',
+                    scope: ['admin']
+                }
+            }
+        });
+
+        const request = { method: 'GET', url: '/', headers: { authorization: 'Bearer ' + token } };
+        const res = await server.inject(request);
+
+        expect(res.result).not.equal('OK');
+        expect(res.statusCode).to.equal(401);
+    });
+
+
+    it('Auth plugin - authorized', async () => {
+
+        const user = internals.users[1];
+        const config = Hoek.clone(internals.config[1]);
+        const token = await HapiJWT.sign(user, config);
+
+        const server = await internals.hapi();
+        server.auth.strategy('jwt', 'hapi-jsonwebtoken', config);
+
+        server.route({
+            method: 'GET',
+            path: '/',
+            handler: function (request, h) {
+
+                return 'OK';
+            },
+            options: {
+                auth: {
+                    strategy: 'jwt',
+                    scope: ['admin']
+                }
+            }
+        });
+
+        const request = { method: 'GET', url: '/', headers: { authorization: 'Bearer ' + token } };
+        const res = await server.inject(request);
+
+        expect(res.result).to.equal('OK');
+        expect(res.statusCode).to.equal(200);
+    });
+
+
+    it('Auth plugin - authorized (two strategies)', async () => {
+
+        const userOne = internals.users[1];
+        const configOne = Hoek.clone(internals.config[1]);
+        const tokenOne = await HapiJWT.sign(userOne, configOne);
+
+        const userTwo = internals.users[2];
+        const configTwo = Hoek.clone(internals.config[2]);
+        const tokenTwo = await HapiJWT.sign(userTwo, configTwo);
+
+        const server = await internals.hapi();
+        server.auth.strategy('jwtOne', 'hapi-jsonwebtoken', configOne);
+        server.auth.strategy('jwtTwo', 'hapi-jsonwebtoken', configTwo);
+
+        server.route({
+            method: 'GET',
+            path: '/endpointOne',
+            handler: function (request, h) {
+
+                return 'OkOne';
+            },
+            options: {
+                auth: 'jwtOne'
+            }
+        });
+
+        server.route({
+            method: 'GET',
+            path: '/endpointTwo',
+            handler: function (request, h) {
+
+                return 'OkTwo';
+            },
+            options: {
+                auth: 'jwtTwo'
+            }
+        });
+
+        server.route({
+            method: 'GET',
+            path: '/endpointThree',
+            handler: function (request, h) {
+
+                return 'OkThree';
+            },
+            options: {
+                auth: {
+                    strategies: ['jwtOne', 'jwtTwo']
+                }
+            }
+        });
+
+        const requestOne = { method: 'GET', url: '/endpointOne', headers: { authorization: 'Bearer ' + tokenOne } };
+        const resOne = await server.inject(requestOne);
+
+        expect(resOne.result).to.equal('OkOne');
+        expect(resOne.statusCode).to.equal(200);
+
+        const requestTwo = { method: 'GET', url: '/endpointTwo', headers: { authorization: 'Bearer ' + tokenTwo } };
+        const resTwo = await server.inject(requestTwo);
+
+        expect(resTwo.result).to.equal('OkTwo');
+        expect(resTwo.statusCode).to.equal(200);
+
+        const requestThree = { method: 'GET', url: '/endpointTwo', headers: { authorization: 'Bearer ' + tokenOne } };
+        const resThree = await server.inject(requestThree);
+
+        expect(resThree.result).not.equal('OkOne');
+        expect(resThree.statusCode).to.equal(401);
+
+        const requestFour = { method: 'GET', url: '/endpointOne', headers: { authorization: 'Bearer ' + tokenTwo } };
+        const resFour = await server.inject(requestFour);
+
+        expect(resFour.result).not.equal('OkOne');
+        expect(resFour.statusCode).to.equal(401);
+
+        const requestFive = { method: 'GET', url: '/endpointThree', headers: { authorization: 'Bearer ' + tokenOne } };
+        const resFive = await server.inject(requestFive);
+
+        expect(resFive.result).to.equal('OkThree');
+        expect(resFive.statusCode).to.equal(200);
+
+        const requestSix = { method: 'GET', url: '/endpointThree', headers: { authorization: 'Bearer ' + tokenTwo } };
+        const resSix = await server.inject(requestSix);
+
+        expect(resSix.result).to.equal('OkThree');
+        expect(resSix.statusCode).to.equal(200);
+    });
+
+
+});
+
+
+
+
+/**
+ * Internals
+ *
+ *
+ */
+internals.config = {
+    1: {
+        secretOrPrivateKey: '#Config01!',
+        sign: {
+            promise: false,
+            options: { // https://github.com/auth0/node-jsonwebtoken#jwtsignpayload-secretorprivatekey-options-callback
+                expiresIn: '1h'
+            }
+        },
+        verify: {
+            promise: false,
+            options: { // https://github.com/auth0/node-jsonwebtoken#jwtverifytoken-secretorpublickey-options-callback
+            }
+        },
+        decode: {
+            promise: false,
+            options: { // https://github.com/auth0/node-jsonwebtoken#jwtdecodetoken--options
+                complete: true
+            }
+        },
+        validate: (request, payload, h) => {
+
+            const user = internals.users[payload.id];
+
+            if (!user) {
+                return { credentials: null, isValid: false };
+            }
+
+            return {
+                isValid: user.valid,
+                credentials: user
+            };
+        }
+    },
+    2: {
+        secretOrPrivateKey: '#Config02!',
+        validate: (request, payload, h) => {
+
+            const user = internals.users[payload.id];
+
+            if (!user) {
+                return { credentials: null, isValid: false };
+            }
+
+            return {
+                isValid: user.valid,
+                credentials: user
+            };
+        }
+    }
+};
+
+internals.hapi = async () => {
+
+    const server = Hapi.server();
+    await server.register(require('../').plugin);
+    return server;
+};
+
+
+internals.users = {
+    1: {
+        id: 1,
+        username: 'axl',
+        valid: true,
+        scope: ['admin']
+    },
+    2: {
+        id: 2,
+        username: 'slash',
+        valid: true,
+        scope: ['admin']
+    },
+    3: {
+        id: 3,
+        username: 'duff',
+        valid: true,
+        scope: ['user']
+    },
+    4: {
+        id: 4,
+        username: 'izzy',
+        valid: false,
+        scope: ['user']
+    }
+};
